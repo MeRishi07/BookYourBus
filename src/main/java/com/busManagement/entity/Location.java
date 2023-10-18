c55bd697ed653697705abadaf4cf4ee33ef812bc
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2023-10-19 00:27:17 +0530

    Add files via upload

---
+++ src/main/java/com/busManagement/entity/Location.java
@@ -0,0 +1,46 @@
+package com.busManagement.entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Location {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	private String terminal;
+	@Column(nullable = false)
+	private String city;
+	@Column(nullable = false)
+	private String state;
+	
+	
+	public Location() {	
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTerminal() {
+		return terminal;
+	}
+	public void setTerminal(String terminal) {
+		this.terminal = terminal;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}}
