eb03692cab05e4e5f24a58d9b9640c7a7fa81b90
Saptarshi <136781153+MeRishi07@users.noreply.github.com>
2023-10-25 22:09:36 +0530

    Delete src directory

--- src/main/java/com/busManagement/Dao/LocationDao.java
+++
@@ -1,7 +0,0 @@
-
-package com.busManagement.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.busManagement.entity.Location;
-public interface LocationDao extends JpaRepository<Location, Long>{
-}
